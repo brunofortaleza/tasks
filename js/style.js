7fad4b575a80c9a108e7fc89a20ffaf5ce63344d
Bruno <brunosilvapalmas@gmail.com>
2020-02-20 12:57:17 -0300

    adicionado cadastrar tarefa com data

--- js/style.js
+++ js/style.js
@@ -7,22 +7,26 @@ class Tarefa {
 
 }
 
-
 function adicionarTarefa(){
 
     var tarefa = document.getElementById("input").value;
     
-    idx = tarefa.indexOf('em')
-    
-
+    idx = tarefa.indexOf("em")
     titulo = tarefa.substr(0, idx - 1);
-    dia = tarefa.substr(idx + 3, );
-
-    dataFormatada = new Date(data);
-
-    console.log(dataFormatada);
+    data = tarefa.substr(idx + 3, );
     
+    // dataFormatada = new Date(data);
 
-    new Tarefa(titulo, dataFormatada, false)
+    taref = new Tarefa(titulo, data, false);
 
+    // localStorage.setItem()
+    
+    // cria o elemento li dentro da ul com os dados
+    var elemento_pai = document.getElementById("myUL");
+    var li_element = document.createElement("li");
+    var text = document.createTextNode(`${taref.data} - ${taref.titulo}`);
+
+    li_element.appendChild(text);
+    elemento_pai.appendChild(li_element);
+    
 }
